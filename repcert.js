37f8c2319d322bae350455b76eea4ecc6b9d9d58
irom77 <irek.romaniuk@gmial.com>
2015-06-28 13:27:53 -0400

    redirection of exec-ssh to string and scheduleJob added

--- repcert.js
+++ repcert.js
@@ -1,52 +1,70 @@
 /**
- * Created by IrekRomaniuk on 5/22/2015.
+ * Created by IrekRomaniuk on 6/22/2015.
  */
 /*
- ResetSic
- #LSMcli mySrvr name pass ResetSic MyROBO aw47q1
- #LSMcli mySrvr name pass ResetSic MyFixedIPROBO sp36rt1 -I=10.20.30.1
  ResetIke
- #LSMcli mySrvr name pass ResetIke MyROBO -CA=OPSEC_CA -R=cer3452s -KEY=ad23fgh
+ #LSMcli mySrvr name pass ResetIke MyROBO -CA=OPSEC_CA -R=cer3452s
  ShowCert
  #cpca_client lscert [-stat Pending|Valid|Revoked|Expired|Renewed] [-kind SIC|IKE|User|LDAP] [-ser ser]
  #cpca_client lscert -stat Valid -kind IKE | grep -A 2 a0008DA73586C
  Run:
- [linux-example]$node repcert.js a0008DA73586C
+ [linux-example]$node repcert.js
      */
-
 var exec = require('ssh-exec');
 var moment = require('moment');
 var config = require('./models/config');
 var email = require('./models/mailer');
-var MyROBO = process.argv[2] || 'a0008DA73586C';
+var MyROBO = [];
 var now = moment(new Date()); //.format("MMMM Do YYYY");
 var days = 30;
 
 require('./models/devices-client')(function callback(err, Hosts) {
     // console.log(Hosts);
-    for (host in Hosts){
-        //if (Hosts[host].hostname == MyROBO)
-        //    console.log(Hosts[host].hostname);
+    for (host in Hosts) {
         var wrapped = moment(Hosts[host].exp_cert);
         var difference = moment(wrapped).diff(now, 'days');
         // console.log(moment(wrapped).fromNow())
-        if (( difference < days) && (difference > 0))
-        //    console.log(Hosts[host].hostname, moment(Hosts[host].exp_cert).format("MMMM Do YYYY"));
-            config.emailText += Hosts[host].hostname + moment(Hosts[host].exp_cert).format("MMMM Do YYYY") + '\n'
+        if (( difference < days) && (difference > 0)) {
+            //    console.log(Hosts[host].hostname, moment(Hosts[host].exp_cert).format("MMMM Do YYYY"));
+            config.emailText += Hosts[host].hostname + ' ' + moment(Hosts[host].exp_cert).format("MMMM Do YYYY") + '\n';
+            MyROBO.push(Hosts[host].hostname);
+        }
     }
-    email.send();
+    email.send();reset()
 });
 
+function reset() {
+    // console.log(MyROBO);
+    var CronJob = require('cron').CronJob;
+    for (robo in MyROBO) {
+        var resetIke = config.resetIke + MyROBO[robo] + ' -CA=' + config.CA;
+        var lscertIKE = config.lscertIKE + MyROBO[robo];
+        scheduleJob(config.envCMA+resetIke+';'+lscertIKE+';', CronJob, MyROBO[robo]);
+    }
+}
 
-var resetSic = config.resetSic + MyROBO + ' ' + config.repSic;
-var resetIke = config.resetIke + MyROBO + ' -CA=' + config.CA;
-// console.log(resetSic + '\n' + resetIke);
-// using ~/.ssh/id_rsa as the private key
-// or edgevpn
-var testcli = 'mdsenv repvpnsc1w;echo $FWDIR';
-var resetIkecli = 'mdsenv repvpnsc1w;' + resetIke;
-// console.log(resetIkecli);
-// exec(resetIkecli, config.user_host).pipe(process.stdout);
+function scheduleJob(command, cron, emailSubject) {
+    var buffers = [];
+    //for testing '0 */1 * * * *' every minute or 'moment().add(5, 'minutes').toDate()'
+    var job = new cron(moment().add(5, 'seconds').toDate(), function(){
+        //console.log(command);
+        stream = process.stdin
+         .pipe(exec(command, config.user_host));
+             stream.on('data', function (buffer) {
+                 buffers.push(buffer);
+             });
+             stream.on('end', function() {
+                 var buffer = Buffer.concat(buffers);
+                 // console.log(buffer.toString());
+                 config.emailSubject = 'attempt to renew VPN cert: ' + emailSubject;
+                 config.emailText = buffer.toString();
+                 email.send();
+                 //process.exit();
+                 // process.stdin.end();
+             });
+    }, true, 'America/Los_Angeles');
+    return job;
+}
 
 
 
